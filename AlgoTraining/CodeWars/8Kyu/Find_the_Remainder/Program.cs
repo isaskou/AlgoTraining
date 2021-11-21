73b0f61f94bfc9330500c124a8ebe40dc957edd6
isaskou <isabel.skou@yahoo.fr>
2021-11-21 13:27:17 +0100

    add CodeWars8Kyu - Find_the_Remainder

--- AlgoTraining/CodeWars/8Kyu/Find_the_Remainder/Program.cs
+++ AlgoTraining/CodeWars/8Kyu/Find_the_Remainder/Program.cs
@@ -4,9 +4,32 @@ namespace Find_the_Remainder
 {
     class Program
     {
+        //Write a function that accepts two arguments and returns the remainder after dividing the larger number by the smaller number.
+
+        //Division by zero should return NaN. 
+
+        //Divison by zero should throw a new DivideByZeroException.
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string divide = "********";
+            //test
+            Console.WriteLine(Remainder(17, 5));
+            Console.WriteLine(divide);
+            Console.WriteLine(Remainder(13, 72));
+            Console.WriteLine(divide);
+            Console.WriteLine(Remainder(1, 0));
+            Console.WriteLine(divide);
+            Console.WriteLine(Remainder(0, 0));
+
+
+            //Mysolution
+            static int Remainder(int a, int b)
+            {
+                if (a == 0 || b == 0) throw new DivideByZeroException();
+                return a > b ? a % b : b % a;
+            }
+
+
         }
     }
 }
