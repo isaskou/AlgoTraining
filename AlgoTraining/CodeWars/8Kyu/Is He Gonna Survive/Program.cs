2d3767e2c79320dba714ca96b03a61b05b8e8ad3
isaskou <isabel.skou@yahoo.fr>
2021-11-21 13:29:45 +0100

    add CodeWars8Kyu - Is_he_gonna_survive

---
+++ AlgoTraining/CodeWars/8Kyu/Is He Gonna Survive/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Is_He_Gonna_Survive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
