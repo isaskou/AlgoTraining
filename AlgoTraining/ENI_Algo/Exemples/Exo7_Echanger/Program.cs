647df880d0ff198976221bb27b130aaa43d86ee0
isaskou <isabel.skou@yahoo.fr>
2021-11-21 12:37:37 +0100

    add Exo7_Echanger

---
+++ AlgoTraining/ENI_Algo/Exemples/Exo7_Echanger/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exo7_Echanger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
