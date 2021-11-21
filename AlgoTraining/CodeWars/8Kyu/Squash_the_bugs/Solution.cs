0ae525bb088cc035a463be47c604e32450e86af3
isaskou <isabel.skou@yahoo.fr>
2021-11-21 13:39:54 +0100

    add CodeWars8Kyu - Squash_the_bugs

---
+++ AlgoTraining/CodeWars/8Kyu/Squash_the_bugs/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Squash_the_bugs
+{
+    public class Solution
+    {
+        public int FindLongest(string str)
+        {
+            var spl = str.Split(" ");
+            var longest = 0;
+
+            for (var i = 0; i < spl.Length; i++)
+            {
+                if (spl[i].Length > longest)
+                {
+                    longest = spl[i].Length;
+                }
+            }
+            return longest;
+        }
+
+
+
+    }
+}
