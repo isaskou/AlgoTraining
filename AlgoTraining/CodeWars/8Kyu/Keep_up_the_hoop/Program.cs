e60d73ebd373a9b2357a7044597e2bf62a482df4
isaskou <isabel.skou@yahoo.fr>
2021-11-21 13:35:20 +0100

    add CodeWars8Kyu - Keep_up_the_hoop

---
+++ AlgoTraining/CodeWars/8Kyu/Keep_up_the_hoop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Keep_up_the_hoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string div = "*******";
+            Solution s = new Solution();
+
+            Console.WriteLine("My Solution");
+
+            Console.WriteLine(s.HoopCount(34));
+
+            Console.WriteLine(div);
+
+            Console.WriteLine("Best Solution");
+
+            Console.WriteLine(s.HoopCount(34));
+
+            static string HoopCount(int n)
+            {
+                return n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it";
+            }
+
+        }
+    }
+}
---
+++ AlgoTraining/CodeWars/8Kyu/Keep_up_the_hoop/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keep_up_the_hoop
+{
+    public class Solution
+    {
+        public string HoopCount(int n)
+        {
+            if (n >= 10)
+            {
+                return "Great, now move on to tricks";
+            }
+            else
+            {
+                return "Keep at it until you get it";
+            }
+        }
+
+    }
+}
