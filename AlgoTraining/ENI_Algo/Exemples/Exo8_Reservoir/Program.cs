03335ae4c7024cbee6f406fe6c13441de61beafa
isaskou <isabel.skou@yahoo.fr>
2021-11-21 12:48:50 +0100

    add exo8_Reservoir in ENIExemples

---
+++ AlgoTraining/ENI_Algo/Exemples/Exo8_Reservoir/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace Exo8_Reservoir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exo8_reservoir");
+
+            //établir la liste des opérations pertinentes applicables à un réservoir
+
+            //voir classe Reservoire
+            string divide = "*********";
+
+            Reservoir r = new Reservoir();
+            r.capacite = 1000;
+            r.contenance = 0;
+            Console.WriteLine("capacité initale : {0} \n contenance initiale : {1}", r.capacite, r.contenance);
+
+            Console.WriteLine("est rempli : " + r.estRempli);
+            Console.WriteLine("est vide : " + r.estVide);
+
+            Console.WriteLine(divide);
+
+            r.Remplir();
+            Console.WriteLine("methode remplir");
+            Console.WriteLine("est rempli : " + r.estRempli);
+            Console.WriteLine("est vide : " + r.estVide);
+
+            Console.WriteLine(divide);
+
+            r.Vider();
+            Console.WriteLine("méthode vider");
+            Console.WriteLine("est rempli : " + r.estRempli);
+            Console.WriteLine("est vide : " + r.estVide);
+
+            Console.WriteLine(divide);
+
+            r.Ajouter(500);
+            Console.WriteLine("méthode ajouter");
+            Console.WriteLine("est rempli : " + r.estRempli);
+            Console.WriteLine("est vide : " + r.estVide);
+            Console.WriteLine("capacité : {0}L", r.capacite);
+            Console.WriteLine("contenance : {0}L", r.contenance);
+
+            Console.WriteLine(divide);
+            r.Enlever(250);
+            Console.WriteLine("méthode enlever");
+            Console.WriteLine("est rempli : " + r.estRempli);
+            Console.WriteLine("est vide : " + r.estVide);
+            Console.WriteLine("capacité : {0}L", r.capacite);
+            Console.WriteLine("contenance : {0}L", r.contenance);
+
+            Console.WriteLine(divide);
+            Console.WriteLine("test des erreurs possibles");
+
+            Console.WriteLine("enlever 500");
+            r.Enlever(500);
+            Console.WriteLine("capacité : {0}L", r.capacite);
+            Console.WriteLine("contenance : {0}L", r.contenance);
+
+
+            Console.WriteLine("ajouter 1500");
+            r.Ajouter(1500);
+            Console.WriteLine("capacité : {0}L", r.capacite);
+            Console.WriteLine("contenance : {0}L", r.contenance);
+
+
+            Console.WriteLine(divide);
+            Console.WriteLine("est rempli : " + r.estRempli);
+            Console.WriteLine("est vide : " + r.estVide);
+            Console.WriteLine("capacité : {0}L", r.capacite);
+            Console.WriteLine("contenance : {0}L", r.contenance);
+
+
+            Console.WriteLine(divide);
+            Console.WriteLine("remplir");
+            r.Remplir();
+            Console.WriteLine("est rempli : " + r.estRempli);
+            Console.WriteLine("est vide : " + r.estVide);
+            Console.WriteLine("capacité : {0}L", r.capacite);
+            Console.WriteLine("contenance : {0}L", r.contenance);
+
+            Console.WriteLine(divide);
+            Console.WriteLine("vider");
+            r.Vider();
+            Console.WriteLine("est rempli : " + r.estRempli);
+            Console.WriteLine("est vide : " + r.estVide);
+            Console.WriteLine("capacité : {0}L", r.capacite);
+            Console.WriteLine("contenance : {0}L", r.contenance);
+
+
+        }
+    }
+}
---
+++ AlgoTraining/ENI_Algo/Exemples/Exo8_Reservoir/Reservoir.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo8_Reservoir
+{
+    public class Reservoir
+    {
+        public double capacite { get; set; }
+        public double contenance { get; set; }
+        private bool _estVide;
+
+        public bool estVide
+        {
+            get
+            {
+                if (contenance == 0) return true;
+                else return false;
+            }
+            set { _estVide = value; }
+        }
+
+
+        private bool _estRempli;
+
+        public bool estRempli
+        {
+            get
+            {
+                if (contenance == capacite) return true;
+                else return false;
+            }
+            set { _estRempli = value; }
+        }
+
+
+        public void Vider()
+        {
+            if (estVide == false)
+            {
+                contenance = 0;
+                estVide = true;
+            }
+            else
+            {
+                Console.WriteLine("c'est déjà vide");
+            }
+        }
+        public void Remplir()
+        {
+            if (estRempli == false)
+            {
+                contenance = capacite;
+                estRempli = true;
+            }
+            else
+            {
+                Console.WriteLine("c'est déjà rempli ! ");
+            }
+        }
+        public void Ajouter(double a)
+        {
+            if (estRempli == true) Console.WriteLine("impossible d'ajouter");
+
+            if ((contenance + a) > capacite) Console.WriteLine("ça va déborder");
+
+            else
+            {
+                contenance = contenance + a;
+            }
+
+        }
+        public void Enlever(double b)
+        {
+            if (estVide == true) Console.WriteLine("impossible d'enlever");
+            if ((contenance - b) < 0) Console.WriteLine("c'est bientôt vide");
+
+            else
+            {
+                contenance = contenance - b;
+            }
+        }
+    }
+}
+
