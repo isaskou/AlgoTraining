16d20ac0cb24bb8c89fd921333a964141008e0db
isaskou <isabel.skou@yahoo.fr>
2021-11-21 13:27:52 +0100

    add CodeWars8Kyu - I_love_you__a_little___a_lot__passionately___not_at_all

---
+++ AlgoTraining/CodeWars/8Kyu/I_love_you__a_little___a_lot__passionately___not_at_all/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace I_love_you__a_little___a_lot__passionately___not_at_all
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Solution s = new Solution();
+            //s.HowMuchILoveYou(142);
+            Console.WriteLine("*******");
+            HowMuchILoveYou(7);
+            //HowMuchILoveYou(3);
+            //HowMuchILoveYou(10);
+            //HowMuchILoveYou(6);
+            HowMuchILoveYou(14);
+
+            static string HowMuchILoveYou(int nb_petals)
+            {
+                string[] messages = { "I love you", "a little", "a lot", "passionately", "madly", "not at all" };
+                int count = 0;
+                for (int i = 0; i < nb_petals; i++)
+                {
+                    count++;
+                    if (count > 6) count = 1;
+                }
+
+                Console.WriteLine(messages[count - 1]);
+                return messages[count - 1];
+            }
+        }
+    }
+}
---
+++ AlgoTraining/CodeWars/8Kyu/I_love_you__a_little___a_lot__passionately___not_at_all/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I_love_you__a_little___a_lot__passionately___not_at_all
+{
+    public class Solution
+    {
+        public string HowMuchILoveYou(int nb_petals)
+        {
+            string[] messages = { "I love you", "a little", "a lot", "passionately", "madly", "not at all" };
+            string result = "";
+            int cpt = 0;
+
+
+            for (int i = 0; i < nb_petals; i++)
+            {
+                if (i >= messages.Length)
+                {
+                    if (cpt >= 6)
+                    {
+                        cpt = 0;
+                    }
+                    result = messages[cpt];
+                    cpt++;
+
+                }
+                else { result = messages[i]; }
+
+            }
+
+            //Console.WriteLine(result);
+            return result;
+        }
+
+    }
+}
