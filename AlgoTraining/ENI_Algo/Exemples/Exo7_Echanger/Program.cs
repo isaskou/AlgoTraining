c213c39ab8534cff5da5c73d453aa5912c8f0d04
isaskou <isabel.skou@yahoo.fr>
2021-11-21 12:41:39 +0100

    update Exo7_Echanger

--- AlgoTraining/ENI_Algo/Exemples/Exo7_Echanger/Program.cs
+++ AlgoTraining/ENI_Algo/Exemples/Exo7_Echanger/Program.cs
@@ -6,7 +6,39 @@ namespace Exo7_Echanger
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("exo7");
+
+            Console.WriteLine("My Solution");
+
+            Echanger(10, 20);
+            Console.WriteLine("*********");
+
+            Console.WriteLine("SolutionEni");
+
+            Echanger2(10, 20);
+
+            static void Echanger(int a, int b)
+            {
+                Console.WriteLine("variable initiales : var1={0}, var2={1}", a, b);
+
+                int varTemp = a;
+                a = b;
+                b = varTemp;
+
+                Console.WriteLine("echanges des valeurs :  var1={0}, var2={1}", a, b);
+
+            }
+
+            static void Echanger2(int a, int b)
+            {
+                Console.WriteLine("variable initiales : var1={0}, var2={1}", a, b);
+
+                a = a + b;
+                b = a - b;
+                a = a - b;
+                Console.WriteLine("echanges des valeurs :  var1={0}, var2={1}", a, b);
+
+            }
         }
     }
 }
