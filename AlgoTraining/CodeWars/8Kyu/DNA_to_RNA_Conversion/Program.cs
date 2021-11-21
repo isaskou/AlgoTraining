b872bb3bef19f30b782386b66193a516e20e0366
isaskou <isabel.skou@yahoo.fr>
2021-11-21 13:21:01 +0100

    add CodeWars8Kyu - Filter_out_the_geese

--- AlgoTraining/CodeWars/8Kyu/DNA_to_RNA_Conversion/Program.cs
+++ AlgoTraining/CodeWars/8Kyu/DNA_to_RNA_Conversion/Program.cs
@@ -6,7 +6,21 @@ namespace DNA_to_RNA_Conversion
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string dna = "GCAT";
+            dnaToRna(dna);
+
+
+            static string dnaToRna(string dna) => dna.Replace("T", "U");
+            //{
+            //    string rna = dna.Replace("T", "U");
+
+            //    return rna;
+            //    //if (dna.Contains("T"))
+            //    //{
+            //    //    rna.Replace("T", "U");
+            //    //}
+            //    //return rna;
+            //}
         }
     }
 }
---
+++ AlgoTraining/CodeWars/8Kyu/Filter_out_the_geese/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Filter_out_the_geese
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
