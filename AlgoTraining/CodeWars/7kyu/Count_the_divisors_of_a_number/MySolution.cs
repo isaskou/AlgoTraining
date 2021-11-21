24e15161441a387cd11f437a9ab8fb9bed260243
isaskou <isabel.skou@yahoo.fr>
2021-11-21 13:06:41 +0100

    add CodeWars 7kyu - Count_the_divisors_of_a_number w/ MySolution

---
+++ AlgoTraining/CodeWars/7kyu/Count_the_divisors_of_a_number/MySolution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_the_divisors_of_a_number
+{
+    public class MySolution
+    {
+        public int Divisors(int n)
+        {
+            int cpt = 0;
+
+            for (int diviseur = 1; diviseur <= n; diviseur++)
+            {
+                if (n % diviseur == 0) cpt++;
+                else continue;
+            }
+
+            return cpt;
+        }
+    }
+}
---
+++ AlgoTraining/CodeWars/7kyu/Count_the_divisors_of_a_number/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Count_the_divisors_of_a_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
