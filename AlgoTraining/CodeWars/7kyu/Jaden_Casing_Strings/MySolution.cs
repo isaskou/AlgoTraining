3fcacdcab4bdd02dafe1da5c1b7842e4e01ef285
isaskou <isabel.skou@yahoo.fr>
2021-11-21 13:44:25 +0100

    add CodeWars7Kyu - Jaden_Casing_Strings

---
+++ AlgoTraining/CodeWars/7kyu/Jaden_Casing_Strings/MySolution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jaden_Casing_Strings
+{
+    public class MySolution
+    {
+        public string ToJadenCase(string phrase)
+        {
+            List<string> array = phrase.Split(' ').ToList();
+            string newWord = "";
+            List<string> newSentence = new List<string>();
+
+            foreach (var item in array)
+            {
+                newWord = item[0].ToString().ToUpper() + item.Substring(1).ToLower();
+                newSentence.Add(newWord);
+            }
+
+            return string.Join(" ", newSentence);
+
+        }
+    }
+}
---
+++ AlgoTraining/CodeWars/7kyu/Jaden_Casing_Strings/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace Jaden_Casing_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "How can mirrors be real if our eyes aren't real";
+            string divide = "*********";
+            Console.WriteLine("Phrase initiale : {0}", phrase);
+
+            Console.WriteLine(divide);
+
+            Console.WriteLine("My solution : ");
+            MySolution s = new MySolution();
+            Console.WriteLine(s.ToJadenCase(phrase));
+
+            Console.WriteLine(divide);
+            Console.WriteLine("Best Practice 1");
+            Console.WriteLine(ToJadenCase_Solution1(phrase));
+
+            Console.WriteLine(divide);
+            Console.WriteLine("Best Practice 2");
+            Console.WriteLine(ToJadenCase_Solution2(phrase));
+
+            static string ToJadenCase_Solution1(string phrase)
+            {
+                return String.Join(" ", phrase.Split().Select(i => Char.ToUpper(i[0]) + i.Substring(1)));
+            }
+
+            static string ToJadenCase_Solution2(string phrase)
+            {
+                return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(phrase);
+            }
+
+
+
+
+        }
+    }
+}
