eef400632da6b0d01ac26074a1cf8e356088bd92
isaskou <isabel.skou@yahoo.fr>
2021-11-21 13:18:36 +0100

    add CodeWars8Kyu - DNA_to_RNA_Conversion

---
+++ AlgoTraining/CodeWars/8Kyu/DNA_to_RNA_Conversion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DNA_to_RNA_Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
