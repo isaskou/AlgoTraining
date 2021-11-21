22c629c9973113dede61b611cd124ef799f2051e
isaskou <isabel.skou@yahoo.fr>
2021-11-21 13:46:59 +0100

    add CodeWars7Kyu - Waiting room --> Very proud, I found the best solution directly

---
+++ AlgoTraining/CodeWars/7kyu/Waiting_room/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Waiting_room
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int chairs = 10;
+            Console.WriteLine(LastChair(chairs));
+
+            static int LastChair(int n)
+            {
+                return n - 1;
+
+            }
+        }
+    }
+}
