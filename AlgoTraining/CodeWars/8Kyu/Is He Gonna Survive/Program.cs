4d2d5e7c08c40f2e2a7d9f5b1aa051b8987d8a90
isaskou <isabel.skou@yahoo.fr>
2021-11-21 13:30:07 +0100

    add CodeWars8Kyu - Is_he_gonna_survive

--- AlgoTraining/CodeWars/8Kyu/Is He Gonna Survive/Program.cs
+++ AlgoTraining/CodeWars/8Kyu/Is He Gonna Survive/Program.cs
@@ -6,7 +6,20 @@ namespace Is_He_Gonna_Survive
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("TestCase");
+            Console.WriteLine(Hero(10, 5));
+            Console.WriteLine("TestCase2");
+            Console.WriteLine(Hero(7, 4));
+            Console.WriteLine("TestCase3");
+            Console.WriteLine(Hero(0, 1));
+            Console.WriteLine("TestCase3");
+            Console.WriteLine(Hero(100, 40));
+
+
+            static bool Hero(int bullets, int dragons)
+            {
+                return (dragons * 2) > bullets ? false : true;
+            }
         }
     }
 }
