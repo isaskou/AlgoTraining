b14c8d482bdb878d02e54fccf66780f29f6ba645
isaskou <isabel.skou@yahoo.fr>
2021-11-22 16:01:43 +0100

    add CodeWars7kyu - Breaking Chocolate Problem

---
+++ AlgoTraining/CodeWars/7kyu/Breaking chocolate problem/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Breaking_chocolate_problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+            #region MySolution
+            string div = "********";
+            #region TestMySolution
+            int n = 2;
+            int m = 1;
+            Console.WriteLine("n = {0} - m = {1} - number of split = {2}", n, m, BreakChocolate(n,m));
+            Console.WriteLine(div+"\n");
+
+            n = 5;m = 5;
+            Console.WriteLine("n = {0} - m = {1} - number of split = {2}", n, m, BreakChocolate(n, m));
+            Console.WriteLine(div);
+
+            #endregion
+
+            static int BreakChocolate(int n, int m)
+            {
+                //int result = (n * m) - 1;
+                //if (result < 0) return 0;
+                //else return result;
+                return ((n * m) - 1) < 0 ? 0 : ((n * m) - 1);
+            }
+            #endregion
+
+            #region BestSolutions
+
+            #region testBestSolution
+            Console.WriteLine("Solution1");
+            Console.WriteLine("n = {0} - m = {1} - number of split = {2}", n, m, Solution1(n, m));
+            Console.WriteLine(div);
+
+            Console.WriteLine("Solution2");
+            Console.WriteLine("n = {0} - m = {1} - number of split = {2}", n, m, Solution2(n, m));
+            Console.WriteLine(div);
+
+
+            #endregion
+            static int Solution1(int a, int b)
+            {
+                //same as me but in different way
+                return a * b > 0 ? a * b - 1 : 0;
+            }
+
+            static int Solution2(int n, int m)
+            {
+                //return the biggest between de 0 and n*m-1
+                return Math.Max(0, n * m - 1);
+            }
+            #endregion
+        }
+    }
+}
---
+++ AlgoTraining/CodeWars/7kyu/Breaking chocolate problemTests/BestSolutionsTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Breaking_chocolate_problem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Breaking_chocolate_problem.Tests
+{
+    [TestClass()]
+    public class BestSolutionsTests
+    {
+        [TestMethod()]
+        public void Solution1Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void Solution2Test()
+        {
+            Assert.Fail();
+        }
+    }
+}
