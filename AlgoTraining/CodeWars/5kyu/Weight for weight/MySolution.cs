148748b48a5bc1d5d8341c0efe23d2d219414df2
isaskou <isabel.skou@yahoo.fr>
2021-12-08 11:01:34 +0100

    add 5kyu - Weight For weight - TODO : test with string[]

---
+++ AlgoTraining/CodeWars/5kyu/Weight for weight/MySolution.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weight_for_weight
+{
+    public class MySolution
+    {
+        public string orderWeight(string strng)
+        {
+            string[] result = new string[] { "" };
+
+            if (String.IsNullOrEmpty(strng))
+            {
+                return "";
+            }
+            
+
+            else
+            {
+                result = strng.Split(' ');
+                Array.Sort(result);
+
+                int[] intArray = Array.ConvertAll(result, x => int.Parse(x));
+
+                List<int> listSum = new List<int>();
+
+                //pour calculer la somme de chq nombre du tableau 
+
+                int sum = 0, number, temp;
+
+                for (int i = 0; i < intArray.Length; i++)
+                {
+                    number = intArray[i];
+                    sum = 0;
+                    temp = 0;
+                    while (number != 0)
+                    {
+                        temp = number % 10;
+                        sum += temp;
+                        number /= 10;
+                    }
+
+                    listSum.Add(sum);
+                }
+
+                //Pour comparer les 2 tableaux
+
+                int temp2;
+
+                for (int i = 0; i < listSum.Count; i++)
+                {
+                    for (int j = 1; j <= listSum.Count - 1; j++)
+                    {
+                        if (listSum[j] < listSum[j - 1])
+                        {
+                            temp2 = intArray[j];
+                            temp = listSum[j];
+                            intArray[j] = intArray[j - 1];
+                            listSum[j] = listSum[j - 1];
+                            intArray[j - 1] = temp2;
+                            listSum[j - 1] = temp;
+                        }
+                    }
+                }
+
+
+
+                //Pour afficher la liste en string
+                string response = "";
+
+                for (int i = 0; i < intArray.Length; i++)
+                {
+                    if (i != intArray.Length - 1)
+                    {
+                        response += intArray[i].ToString() + " ";
+
+                    }
+                    else
+                    {
+                        response += intArray[i].ToString();
+                    }
+
+
+                }
+                //foreach (var item in intArray)
+                //{
+                //    response += item.ToString()+" ";
+                //}
+
+                return response;
+            }
+
+
+
+
+        }
+
+
+
+
+
+    }
+}
+
---
+++ AlgoTraining/CodeWars/5kyu/Weight for weight/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Weight_for_weight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MySolution s = new MySolution();
+            Console.WriteLine(s.orderWeight(null));
+
+            int x, y, temp;
+
+            x = 10;
+            y = 5;
+            Console.WriteLine("x={0} - y={1}", x, y);
+
+            temp = x;
+            x = y;
+            y = temp;
+            Console.WriteLine("x={0} - y={1}", x, y);
+
+        }
+    }
+}
